eab47da253395651375f84d8f74aa01a5827378b
brwnn0 <112787390+brwnn0@users.noreply.github.com>
2022-09-03 22:17:45 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var text = prompt('Enter your text', ''); // Message text
+var pass = prompt('Enter your passwowrd', ''); // Passwordw
+var pin1 = prompt('Insert the first pin with 2 digits', ''); // First multiplier digit 
+var pin2 = prompt('Insert the second pin with 2 digits', ''); // Second multiplier digit 
+var pin3 = prompt('insert the third pin with 2 digits', ''); // Third multiplier digit 
+var pin4 = prompt('insert the fourth pin with 2 digits', ''); // Fourth multiplier digit 
