511b7cf7d79d76f3c45153ca4a5a75facce0ec9e
brwnn0 <112787390+brwnn0@users.noreply.github.com>
2022-09-05 11:41:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ var pin1 = prompt('Insert the first pin with 2 digits', ''); // First multiplier
 var pin2 = prompt('Insert the second pin with 2 digits', ''); // Second multiplier digit 
 var pin3 = prompt('insert the third pin with 2 digits', ''); // Third multiplier digit 
 var pin4 = prompt('insert the fourth pin with 2 digits', ''); // Fourth multiplier digit 
+
+const char = ["A", "0",]
+const index = char.indexOf("A");
+if (index !== -1) {
+    char[index] = "01000001";
+}
+
+
+char
